25a6a5ba7e7bba9a65018d65aabdfa30d298cd91
Ritesh7767 <ritesh776782@gmail.com>
2024-05-28 03:22:37 +0530

    SubscriptionModel

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new Schema({
     },
     watchHistory : [
         {
-            types : Schema.Types.ObjectId,
+            type : Schema.Types.ObjectId,
             ref : "Video"
         }
     ],
@@ -58,7 +58,7 @@ userSchema.pre("save", async function(next){
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.passwor)
+    return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function(){
